2711e1ec3925b9102332b08646330a640005f2c6
od3ng <lepengdados@gmail.com>
2018-11-05 09:47:02 +0700

    updated
    1. add folder admin,home,auth
    2. add file .html,.css

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,4 @@
 # instance/config.py
 
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/dreamteam_db'
+SQLALCHEMY_DATABASE_URI ='mysql+pymysql://root:@localhost/dreamteam_db?charset=utf8'
